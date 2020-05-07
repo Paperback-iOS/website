30f99483d2dcd6ae174ab153c9775b5011098b8a
Soitora <simon.mattila@protonmail.com>
2020-05-07 11:48:19 +0200

    Remove experiments

--- src/.vuepress/config/nav/en.js
+++ src/.vuepress/config/nav/en.js
@@ -1,31 +1,5 @@
 module.exports = [
 	{ text: "Home", link: "/" },
-	{
-		text: "Experiments",
-		items: [
-			{
-				text: "Experiments",
-				items: [
-					{
-						text: "1",
-						link: "/experiment/1/",
-					},
-					{
-						text: "2",
-						link: "/experiment/2/",
-					},
-					{
-						text: "3",
-						link: "/experiment/3/",
-					},
-					{
-						text: "4",
-						link: "/experiment/4/",
-					},
-				],
-			},
-		],
-	},
 	{ text: "FAQ", link: "/help/faq/" },
 	{
 		text: "Guides",
