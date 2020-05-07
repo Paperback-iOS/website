7a2c96906c04c4443c05e9fb77ac6865ccea2bc0
Soitora <simon.mattila@protonmail.com>
2020-05-07 11:02:16 +0200

    Experiment

--- src/.vuepress/config/nav/en.js
+++ src/.vuepress/config/nav/en.js
@@ -1,5 +1,31 @@
 module.exports = [
-	{ text: 'Home', link: '/' },
+	{ text: "Home", link: "/" },
+	{
+		text: "Experiments",
+		items: [
+			{
+				text: "Experiments",
+				items: [
+					{
+						text: "1",
+						link: "/experiment/1/",
+					},
+					{
+						text: "2",
+						link: "/experiment/2/",
+					},
+					{
+						text: "3",
+						link: "/experiment/3/",
+					},
+					{
+						text: "4",
+						link: "/experiment/4/",
+					},
+				],
+			},
+		],
+	},
 	{ text: "FAQ", link: "/help/faq/" },
 	{
 		text: "Guides",
