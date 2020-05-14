323095fbf8b0eca5ff5df979f782b2a1e9261982
Framboise <25133304+FramboisePi@users.noreply.github.com>
2020-05-14 21:54:24 +0200

    add: notuptodate vue component

---
+++ src/.vuepress/components/notuptodate.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="custom-block warning" v-if='this.$page.frontmatter.lang == "fr-FR"'>
+  <p class="custom-block-title">
+    Cette page n’est pas à jour.
+  </p> 
+  <p>
+    D’importantes modifications ont été faites dans la version anglaise de cette. Nous vous recommandons d’utiliser celle-ci.
+  </p>
+</div>
+<div class="custom-block warning" v-else>
+  <p class="custom-block-title">
+    This page isn't up to date.
+  </p> 
+  <p>
+    Major changes have been made in the English version of this page. We suggest you to use this one.
+  </p>
+</div>
+</template>
