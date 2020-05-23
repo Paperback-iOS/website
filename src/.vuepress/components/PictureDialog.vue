700006a6c25d553ccfeac143f76263fba0784665
Framboise <25133304+FramboisePi@users.noreply.github.com>
2020-05-23 17:48:27 +0200

    support two dialog sizes

--- src/.vuepress/components/PictureDialog.vue
+++ src/.vuepress/components/PictureDialog.vue
@@ -1,36 +1,47 @@
 <template>
 	<span>
-        <a @click="dialogVisible = true" style="cursor:pointer;">{{ button }}</a>
+		<a @click="dialogVisible = true" style="cursor:pointer;">{{ button }}</a>
 
 		<el-dialog :title="title" 
 		:visible.sync="dialogVisible"
 		center
-		width="65%">
+		:width="size">
 			<img :src="src"/>
 		</el-dialog>
 	</span>
 </template>
 
 <script>
-  export default {
-  props: {
+export default {
+	props: {
 		title: {
 			type: String,
 			required: true,
 		},
-        button: {
+		button: {
 			type: String,
 			required: true,
 		},
-        src: {
+		src: {
 			type: String,
 			required: true,
 		},
 	},
-    data() {
-      return {
-        dialogVisible: false,
-      };
-    }
+	data() {
+		// If the sidebar is present, use width = 65% for the dialog. 
+		// Else use width = 90% for a better readability on small screens
+		if (screen.width > 767) {
+			return {
+				dialogVisible: false,
+				size: "65%"
+			}
+		} 
+		else {
+			return {
+				dialogVisible: false,
+				size: "90%"
+			}
+		}
+	},
   };
 </script>
